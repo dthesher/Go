32f43edc76887761e6c364e1a1423b5a3f89052e
ace_d <dthesher@gmail.com>
2023-11-11 18:34:04 +0530

    clear console

--- util/util.go
+++ util/util.go
@@ -1,1 +1,23 @@
 package util
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"runtime"
+)
+
+func ClearConsole() {
+	switch runtime.GOOS {
+	case "linux", "darwin":
+		cmd := exec.Command("clear")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	case "windows":
+		cmd := exec.Command("cmd", "/c", "cls")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	default:
+		fmt.Println("Unsupported operating system")
+	}
+}
